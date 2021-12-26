2d549fd17efd66e5f818eb6fce04dec114bbe802
Ryunosuke Yamamoto <s1f101801453@iniad.org>
2021-12-26 22:14:06 +0900

    add groomings and LP

---
+++ js/index.js
@@ -0,0 +1,13 @@
+$(function(){
+
+  $("a[href*=#]:not([href=#])").click(function(){
+  
+    var target = $($(this).attr("href")).offset().top;
+
+    target -= 70;
+
+    $("html, body").animate({scrollTop : target}, 500);
+
+    return false;
+  });
+});
